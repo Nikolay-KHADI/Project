ddc13c7ee58e006f2346a03eae9c37f86e799d64
Nikolay-KMS <karpuk.nik@gmail.com>
2023-11-16 14:36:43 +0200

    fix 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@
 import './App.css'
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
+import CssBaseline from '@mui/material/CssBaseline';
 
 
 export function App() {
 
   return (
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </Provider>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
-import CssBaseline from '@mui/material/CssBaseline';
+
 
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <CssBaseline />
     <App />
-  </Provider>
-  // </React.StrictMode>,
 )
